32985d1eb228df4b5ae1097eedeaba6fe22a81a1
Yusri <yusri.shakir@gmail.com>
2020-10-12 15:05:46 +0200

    added funktionallity

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Hangman
 {
@@ -9,25 +10,48 @@ namespace Hangman
 
             var wrongLetter = new char[10];
             string rightAnswer = "MAURITZ";
+            char[] rightAswerA = new char[rightAnswer.Length]; 
             int numberOfGuesses = 10;
+            char[] rightGuesses = new char[] {'_','_','_','_','_','_','_'};
 
             bool correctGuess = false;
             char guess;
 
+            rightAswerA = rightAnswer.ToCharArray();
+
+            
+            
+
             do
             {
+                foreach (var item in rightGuesses)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
                 Console.WriteLine("Enter your guess: ");
                 guess = char.Parse(Console.ReadLine());
                 if (rightAnswer.Contains(guess))
                 {
+                    rightGuesses[rightAnswer.IndexOf(guess)] = guess;
                     Console.WriteLine($"{guess} is correct");
                 }
+
                 else
                 {
-                    wrongLetter[numberOfGuesses] = guess;
+                    wrongLetter[numberOfGuesses -1] = guess;
                     Console.WriteLine($"{guess} is wrong");
+                    numberOfGuesses--;
+
                 }
-                numberOfGuesses--;
+                Console.WriteLine($"You have {numberOfGuesses} guesses left");
+                if (rightAswerA.SequenceEqual(rightGuesses))
+                {
+                    correctGuess = true;
+                    Console.WriteLine("You win");
+
+                }
+                
 
             } while (numberOfGuesses > 0 && correctGuess==false);
         }
