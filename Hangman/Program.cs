0d20796b613e6ab82cd9ec7fe21f6c60fefd40d4
Oscar Olsson <happybits@gmail.com>
2020-10-12 13:39:31 +0200

    -

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,6 +7,7 @@ namespace Hangman
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello from Oscar");
         }
     }
 }
